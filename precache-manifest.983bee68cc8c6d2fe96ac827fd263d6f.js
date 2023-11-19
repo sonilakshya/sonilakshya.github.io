4a2fddc7da39de887db9fecc6f6155bd311df630
sonilakshya <lakshyasoni2703@gmail.com>
2023-11-19 13:53:51 +0530

    new-version of website

---
+++ precache-manifest.983bee68cc8c6d2fe96ac827fd263d6f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f38f0ee186954745bb1bc97f5125fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7b8420fe924168fea48",
+    "url": "/static/css/2.3ab4c7df.chunk.css"
+  },
+  {
+    "revision": "6f9e4be2fbcb5aa6b035",
+    "url": "/static/css/main.44af401b.chunk.css"
+  },
+  {
+    "revision": "c7b8420fe924168fea48",
+    "url": "/static/js/2.ce39de1e.chunk.js"
+  },
+  {
+    "revision": "53e5de17b01785e42125",
+    "url": "/static/js/3.6c5d1d50.chunk.js"
+  },
+  {
+    "revision": "6f9e4be2fbcb5aa6b035",
+    "url": "/static/js/main.9f9b7880.chunk.js"
+  },
+  {
+    "revision": "8e98dbeb45086013edcc",
+    "url": "/static/js/runtime~main.4e525951.js"
+  },
+  {
+    "revision": "7984a2fedcf0486e9e429ae4721787b0",
+    "url": "/static/media/AvertaCY-Bold.7984a2fe.ttf"
+  },
+  {
+    "revision": "53e91c6c3724e5f619923c943891b737",
+    "url": "/static/media/AvertaCY-Light.53e91c6c.ttf"
+  },
+  {
+    "revision": "eb0e7a3cd4179917f9abaf39f3f1ff0b",
+    "url": "/static/media/AvertaCY-Regular.eb0e7a3c.ttf"
+  },
+  {
+    "revision": "183522fe98a295ee91d39babb63bf49c",
+    "url": "/static/media/AvertaCY-Semibold.183522fe.ttf"
+  },
+  {
+    "revision": "200646459c09ea527030f71f3c5dad1f",
+    "url": "/static/media/main_logo.20064645.png"
+  }
+]);
